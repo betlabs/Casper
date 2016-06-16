a6bef1b26e38f3adf6432eed966a03f2f9408a27
Andrey Pavlov <andreo@protonmail.ch>
2016-06-16 17:09:15 +0300

    charset

--- src/Components/ContactForm/form.js
+++ src/Components/ContactForm/form.js
@@ -104,7 +104,7 @@ var ContactForm = function (options) {
 		$.ajax({
 			   method: "POST",
 			   url: "/send-request",
-			   contentType: 'application/json',
+			   contentType: 'application/json; charset=utf-8',
 			   data: JSON.stringify(data),
 			   success: function(){
 					var activeElem = $('.email-form-step.active');
